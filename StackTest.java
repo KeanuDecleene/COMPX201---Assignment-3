b28f26d33f6387aa4965cc3b24db8c61affc0fcb
KeanuDecleene <keanudecleene124@gmail.com>
2024-05-06 17:35:40 +1200

    Lots of Testing

--- StackTest.java
+++ StackTest.java
@@ -1,7 +1,8 @@
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Assertions.*;
 import java.io.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
 
 public class StackTest {
     private Stack stackTest;
@@ -9,26 +10,131 @@ public class StackTest {
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
     @BeforeEach
-    @DisplayName("Resetting stack before each Test")
     public void resetStack() {
         stackTest = new Stack();
         System.setOut(new PrintStream(outputStream));
     }
 
-    //Push function Tests
+    //Push function tests
+
+    /*
+     * Testing pushing to an empty stack
+     */
     @Test
-    @DisplayName("Stack is pushing correctly")
-    public void testPushStack() {
-        buildStack();
+    @DisplayName("Push to empty stack")
+    public void testPushToEmptyStack() {
+        stackTest.push("A");
         stackTest.dump();
-        Assertions.assertEquals("A->B->C->D", outputStream.toString().trim());
+        Assertions.assertEquals("A", stackTest.pop());
     }
 
+    /*
+     * Testing pushing multiple items to a stack and if the order is maintained
+     */
+    @Test 
+    @DisplayName("Multiple pushes maintaining correct order")
+    public void testPush(){
+        buildStack();
+        stackTest.dump();
+        Assertions.assertEquals("D->C->B->A->", outputStream.toString().trim());
+        Assertions.assertEquals("D", stackTest.peek());
+    }
 
+    //Pop function tests
+
+    /*
+     * testing pop function when stack is empty
+     */
+    @Test
+    @DisplayName("Empty stack Pop")
+    public void testEmptyPop(){
+        Assertions.assertNull(stackTest.pop());
+    }
+
+    /*
+     * testing pop function returns correct item and if item is removed correctly
+     */
+    @Test
+    @DisplayName("Non-empty stack Pop")
+    public void testPop(){
+        buildStack();
+        Assertions.assertEquals("D", stackTest.pop());
+        stackTest.dump();
+        Assertions.assertEquals("C->B->A->", outputStream.toString().trim());
+
+    }
+
+    //Peek function tests
+
+    /*
+     * testing peek function when stack is empty
+     */
+    @Test
+    @DisplayName("Empty stack peek")
+    public void testEmptyPeek(){
+        Assertions.assertNull(stackTest.peek());
+    }
+
+    /*
+     * testing peek function when stack is not empty
+     */
+    @Test
+    @DisplayName("Non-empty stack peek")
+    public void testPeek(){
+        buildStack();
+        Assertions.assertEquals("D", stackTest.peek());
+        stackTest.pop();
+        Assertions.assertEquals("C", stackTest.peek());
+    }
+    
+    //isEmpty function tests
+
+    /*
+     * Tests if a stack is empty
+     */
+    @Test
+    @DisplayName("Empty stack returns True")
+    public void testEmptyStack(){
+        Assertions.assertTrue(stackTest.isEmpty());
+    }
+
+    /*
+     * tests that a stack that is not empty returns false 
+     */
+    @Test
+    @DisplayName("Non-empty stack returns False")
+    public void testStack(){
+        buildStack();
+        Assertions.assertFalse(stackTest.isEmpty());
+    }
+
+    //Dump function tests
+
+    /*
+     * Tests that a stack that is empty dumps an empty string
+     */
+    @Test
+    @DisplayName("Empty stack dump")
+    public void testEmptyDump(){
+        stackTest.dump();
+        Assertions.assertEquals("", outputStream.toString().trim());
+    }
     
 
     /*
-     * Builds the stack with some string values.
+     * Tests that a stack that is not empty dumps the correct string
+     */
+    @Test
+    @DisplayName("Non-empty stack dump")
+    public void testNonEmptyDump(){
+        buildStack();
+        stackTest.dump();
+        Assertions.assertEquals("D->C->B->A->", outputStream.toString().trim());
+    }
+
+
+    /*
+     * Builds the stack by pushing some string values.
      */
     private void buildStack(){
         stackTest.push("A");
@@ -37,5 +143,3 @@ public class StackTest {
         stackTest.push("D");
     }
 }
-
-
